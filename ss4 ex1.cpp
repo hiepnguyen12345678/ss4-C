1c62ce98565ef0c31ad1aa5264f9b1bb71a270d2
hiepnguyen12345678 <hiepnguyn4@gmail.com>
2024-11-24 23:53:51 +0700

    Add files via upload

---
+++ ss4 ex1.cpp
@@ -0,0 +1,9 @@
+	scanf("%d", &number);
+	if(number > 0){
+		printf("%d la so nguyen duong", number);
+	}else if(number == 0){
+		printf("%d khong la so nguyen duong, khong la so nguyen am", number);
+	}
+	else{
+		printf("%d la so nguyen am", number);
+
